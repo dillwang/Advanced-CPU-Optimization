10aabba685e7882cb55ade83304bbc22242a5de5
Joachim Galil <jgalil@ucsd.edu>
2025-02-12 18:33:40 -0800

    Moved reg renaming logic to reg file
    Still need work.
    Still ironing out circular queue logic, once that is done it should be set to start working on OoO and retirement.

--- mips_cpu/mips_core/reg_file.sv
+++ mips_cpu/mips_core/reg_file.sv
@@ -34,6 +34,80 @@ module reg_file (
 	reg_file_output_ifc.out out
 );
 
+
+
+
+//Register Renaming stuff
+	parameter int NUM_ARCH_REGS = 32;
+	parameter int NUM_PHYS_REGS = 64;
+	parameter int INTR_QUEUE_SIZE = 16;
+
+	typedef struct {
+		logic size;
+		logic head;
+		logic capacity;
+		logic tail = ((head + size) % capacity);
+	} circ_fifo_t;
+
+	logic [`DATA_WIDTH - 1 : 0] rmt [NUM_ARCH_REGS];
+
+	circ_fifo_t f_list = '{32, 0, NUM_PHYS_REGS - NUM_ARCH_REGS};
+	logic [5:0] free_list [NUM_PHYS_REGS - NUM_ARCH_REGS];
+
+
+	//TODO: these functions dont have logic to prevent ouroboros condition
+
+	function enqueue(input logic [5:0] element, input circ_fifo_t circ,
+		 input logic [5:0] list [NUM_PHYS_REGS - NUM_ARCH_REGS])
+		list[circ.tail] = element;
+		circ.tail = (circ.tail + 1) % circ.capacity;
+		circ.size++;
+	endfunction
+
+	function logic dequeue( input circ_fifo_t circ,
+		 input logic [5:0] list [NUM_PHYS_REGS - NUM_ARCH_REGS])
+		 logic int temp = list[circ.head];
+		 circ.head = (circ.head + 1) % circ.capacity;
+		 circ.size--;
+		return temp;
+	endfunction
+
+
+
+	typedef struct {
+		logic [31:0] instruction;
+		logic [5:0] rd_phys;
+		logic [5:0] rt_phys;
+		logic [5:0] rs_phys;
+		logic valid;
+	} Instr_Queue_Entry_t;
+
+	Instr_Queue_Entry_t instr_queue[INTR_QUEUE_SIZE];
+	instr_head = 0;
+
+
+	function logic [5:0] fetch_free()
+		logic [5:0] new_reg = dequeue(f_list, free_list);
+		return new_reg;
+	endfunction
+
+	
+    //allocate new free physreg
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if(~rst_n) begin
+            for(int i = 0, i < NUM_ARCH_REGS i++) begin
+                free_list[i] = i;
+            end
+        end
+        else begin
+		
+		
+		
+		end
+	end
+
+
 	logic [`DATA_WIDTH - 1 : 0] regs [32];
 
 	assign out.rs_data = i_decoded.uses_rs ? regs[i_decoded.rs_addr] : '0;
@@ -45,5 +119,7 @@ module reg_file (
 			regs[i_wb.rw_addr] = i_wb.rw_data;
 		end
 	end
+
+
 
 endmodule
